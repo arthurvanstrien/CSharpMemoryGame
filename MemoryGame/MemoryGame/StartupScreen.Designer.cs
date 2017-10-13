d2913b5cc03626ab27974d518def4977eccafe2a
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-10-13 11:10:07 +0200

    Added the join game button, the IP field and port field. The IP field and port field have placeholders.

--- MemoryGame/MemoryGame/StartupScreen.Designer.cs
+++ MemoryGame/MemoryGame/StartupScreen.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.HostGameButton = new System.Windows.Forms.Button();
+            this.JoinGameButton = new System.Windows.Forms.Button();
+            this.IPField = new System.Windows.Forms.TextBox();
+            this.PortField = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // HostGameButton
@@ -40,20 +43,50 @@
             this.HostGameButton.Text = "Host Game";
             this.HostGameButton.UseVisualStyleBackColor = true;
             // 
+            // JoinGameButton
+            // 
+            this.JoinGameButton.Location = new System.Drawing.Point(13, 82);
+            this.JoinGameButton.Name = "JoinGameButton";
+            this.JoinGameButton.Size = new System.Drawing.Size(147, 33);
+            this.JoinGameButton.TabIndex = 1;
+            this.JoinGameButton.Text = "Join Game";
+            this.JoinGameButton.UseVisualStyleBackColor = true;
+            // 
+            // IPField
+            // 
+            this.IPField.Location = new System.Drawing.Point(197, 82);
+            this.IPField.Name = "IPField";
+            this.IPField.Size = new System.Drawing.Size(272, 29);
+            this.IPField.TabIndex = 2;
+            // 
+            // PortField
+            // 
+            this.PortField.Location = new System.Drawing.Point(508, 82);
+            this.PortField.Name = "PortField";
+            this.PortField.Size = new System.Drawing.Size(100, 29);
+            this.PortField.TabIndex = 3;
+            // 
             // StartupScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(901, 726);
+            this.Controls.Add(this.PortField);
+            this.Controls.Add(this.IPField);
+            this.Controls.Add(this.JoinGameButton);
             this.Controls.Add(this.HostGameButton);
             this.Name = "StartupScreen";
             this.Text = "StartupScreen";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button HostGameButton;
+        private System.Windows.Forms.Button JoinGameButton;
+        private System.Windows.Forms.TextBox IPField;
+        private System.Windows.Forms.TextBox PortField;
     }
 }
--- MemoryGame/MemoryGame/StartupScreen.cs
+++ MemoryGame/MemoryGame/StartupScreen.cs
@@ -9,7 +9,8 @@ namespace MemoryGame
         {
             InitializeComponent();
 
-            HostGameButton.Click += new System.EventHandler(HostGameButton_Click);
+            //This button handles the start of a game that players can join.
+            HostGameButton.Click += new EventHandler(HostGameButton_Click);
             void HostGameButton_Click(object sender, EventArgs e)
             {
                 //Opens the new window.
@@ -18,6 +19,48 @@ namespace MemoryGame
 
                 //Hides the startup window
                 Hide();
+            }
+
+            //This button handles the joining of player to a game.
+            JoinGameButton.Click += new EventHandler(JoinGameButton_Click);
+            void JoinGameButton_Click(object sender, EventArgs e)
+            {
+                
+
+                //Opens the new window.
+                Game gamePanel = new Game();
+                gamePanel.Show();
+
+                //Hides the startup window
+                Hide();
+            }
+
+            string ipFieldPlaceholder = "IP or domain";
+            IPField.Text = ipFieldPlaceholder;
+            IPField.GotFocus += new EventHandler(ClearIPField);
+            void ClearIPField(object sender, EventArgs e)
+            {
+                IPField.Text = "";
+            }
+            IPField.LostFocus += new EventHandler(FillIPField);
+            void FillIPField(object server, EventArgs e)
+            {
+                if (IPField.Text == "")
+                    IPField.Text = ipFieldPlaceholder;
+            }
+
+            string portFieldPlaceholder = "port";
+            PortField.Text = portFieldPlaceholder;
+            PortField.GotFocus += new EventHandler(ClearPortField);
+            void ClearPortField(object sender, EventArgs e)
+            {
+                PortField.Text = "";
+            }
+            PortField.LostFocus += new EventHandler(FillPortField);
+            void FillPortField(object sender, EventArgs e)
+            {
+                if (PortField.Text == "")
+                    PortField.Text = portFieldPlaceholder;
             }
         }
     }
