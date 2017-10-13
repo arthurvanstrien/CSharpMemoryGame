0c8eb6ad7abd2d11ea9c7d7dc95f97277d808e7b
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-10-13 15:08:00 +0200

    When the hostGame method with the width and height of the panel is called, it now automatically generates a list of shuffled cards with help from the cards class.

---
+++ MemoryGame/MemoryGame/Cards.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MemoryGame
+{
+    class Cards
+    {
+        List<string> images;
+
+        public Cards()
+        {
+            images = new List<string>
+            {
+                "1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20"
+            };
+        }
+
+        public List<string> getRandomCards(int numberOf)
+        {
+            List<string> cards = new List<string>();
+            images = images.OrderBy(a => Guid.NewGuid()).ToList();
+
+            for (int i = 0; i < numberOf; i++)
+            {
+                cards.Add(images[i]);
+                cards.Add(images[i]);
+            }
+
+            cards = cards.OrderBy(a => Guid.NewGuid()).ToList();
+
+            return cards;
+        }
+    }
+}
---
+++ MemoryGame/MemoryGame/HostGame.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    class HostGame
+    {
+        public HostGame(int x, int y)
+        {
+            List<string> cards = new Cards().getRandomCards(x * y);
+        }
+
+        
+
+    }
+}
